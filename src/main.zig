2fa7bc56192874bbf1610b30871ebcf2229bac38
Rohan Vashisht <rohanprogrammer1@gmail.com>
2026-07-24 00:23:04 +0530

    Testing printing all the root declarations

--- src/main.zig
+++ src/main.zig
@@ -12,13 +12,29 @@
 //! - Extract all the doc comments, functions, structs,
 //!   and anything that is in the root directory.
 //! - Render it as html to stdout.
-
-
+//! - There are __INSERT_ labels in the html file,
+//!   on which I will replace it with the documentation.
 
 const std = @import("std");
 
+const test_code: [:0]const u8 = @embedFile("main.zig");
 
+pub fn parse(source: [:0]const u8) !void {
+    var ast = try std.zig.Ast.parse(
+        std.heap.page_allocator,
+        source,
+        .zig,
+    );
 
-pub fn main(init: std.process.Init) !void {
-    init.
+    defer ast.deinit(std.heap.page_allocator);
+
+    for (ast.rootDecls()) |decl| {
+        const main_token = ast.nodeMainToken(decl);
+        const name = ast.tokenSlice(main_token + 1);
+        std.debug.print("{s}\n", .{name});
+    }
+}
+
+pub fn main(_: std.process.Init) !void {
+    try parse(test_code);
 }
