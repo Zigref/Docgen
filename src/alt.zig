6465931480b9f3043664fd9518469cd7e47e3ad3
Rohan Vashisht <rohanprogrammer1@gmail.com>
2026-08-05 19:34:13 +0530

    Added algorithm flow to the comment.

--- src/alt.zig
+++ src/alt.zig
@@ -1,9 +1,46 @@
+//! ======================================
+//! =   _____ _         ____         __  =
+//! =  |__  /(_)  __ _ |  _ \  ___  / _| =
+//! =    / / | | / _` || |_) |/ _ \| |_  =
+//! =   / /_ | || (_| ||  _ <|  __/|  _| =
+//! =  /____||_| \__, ||_| \_\\___||_|   =
+//! =            |___/                   =
+//! ======================================
+//!
 //! I just thought, what if I first remove
 //! the body content of each function.
 //! And store only the declarations, before
 //! the parsing even begins.
 //! Also, the source code has gone unnesecarilly complex.
 //! I am starting to comment each and every single thing.
+//!
+//!
+//!
+//! Ok, I need to think about a very specific algorithmm:
+//!
+//! Hmm, ( O-O')
+//! We will:
+//!     - First remove bodies of all functions.
+//!     - Loop over root declarations:
+//!         TAG_1(namespace_if_present):
+//!         - We will read each tag:
+//!             - if the tag is private:
+//!                  - skip that entire declaration + definition.
+//!                  - No need to check whats nested inside.
+//!                  - Because a pub inside a private, is anyways
+//!                    not accessible.
+//!             - else:
+//!                  - Store the line number.
+//!                  - Store the comment if present.
+//!                  - if the tag is a fn/variable/constant.
+//!                     - add it to documentation
+//!                   - else if tag is a struct/enum/union/opaque(if it is a container type).
+//!                     - if it has fields (in zig these are public if the struct is public) store them.
+//!                     - if it has anything after the fields, something like functions, or constants.
+//!                         - You can think that part like a seperate file of zig.
+//!                         - Hence I will goto TAG_1(with the namespace).
+//!                         - Hence, this becomes recursive.
+//!
 
 const std = @import("std");
 
@@ -53,6 +90,76 @@ pub fn make_all_function_body_empty(gpa: std.mem.Allocator, source: [:0]const u8
 
     try result.appendSlice(gpa, ast.source[cursor..]);
     return result.toOwnedSliceSentinel(gpa, 0);
+}
+
+/// This function returns the doc comment associated
+/// with a declaration. If no doc comment is written, it returns null.
+/// Parameters:
+///     allocator
+///     ast
+///     decl: the declaration whose comment would be returned.
+/// Returns:
+///     The comment of the declaration or null if no comment present.
+pub fn capture_doc_comment(allocator: std.mem.Allocator, ast: std.zig.Ast, decl: std.zig.Ast.Node.Index) ?[]const u8 {
+    // firstToken returns the index of the declared token *Inside* the AST.
+    const first_token_of_the_identifier_declared = ast.firstToken(decl);
+    // ok, now that I got the first
+    // token, I will start going backwards.
+    // While moving backwards, I will check
+    // if the token type is still a comment?
+    // The moment the next character is
+    // not a comment we stop.
+    var current_token = first_token_of_the_identifier_declared;
+
+    while (current_token > 0) { // to make sure I don't go to 0 index. (*o*)
+
+        // because if I go to 0, (O-O)
+        const previous = current_token - 1; // <- this thing becomes -1. (X_X)
+
+        switch (ast.tokenTag(previous)) {
+            .doc_comment, .container_doc_comment => {
+                current_token = current_token - 1;
+            },
+            else => {
+                break;
+            },
+        }
+    }
+
+    if (current_token == first_token_of_the_identifier_declared) {
+        // means, there was no comment to loop over.
+        // hence, our loop exited in the very first iteration.
+        // hence, both the variables are same still.
+        return null;
+        // stating that we didn't find a comment.
+    }
+
+    const the_token_from_which_the_comment_starts = current_token;
+    // the comment length does from:
+    // the_token_from_which_the_comment_starts
+    // till the
+    // first_token_of_the_identifier_declared - 1
+
+    // the tokenStart returns the starting character index of the token you are at.
+    const index_where_identifier_declared = ast.tokenStart(first_token_of_the_identifier_declared);
+    const index_where_comment_declared = ast.tokenStart(the_token_from_which_the_comment_starts);
+    const comment_as_source_code = ast.source[index_where_comment_declared..index_where_identifier_declared];
+
+    var splitted_lines = std.mem.splitScalar(u8, comment_as_source_code, '\n');
+
+    var resultant_comment: std.ArrayList(u8) = .empty;
+
+    while (splitted_lines.next()) |next_line| {
+        if (std.mem.startsWith(u8, next_line, "///")) {
+            resultant_comment.appendSlice(allocator, next_line[3..]) catch return null;
+        } else if (std.mem.startsWith(u8, next_line, "//!")) {
+            resultant_comment.appendSlice(allocator, next_line[3..]) catch return null;
+        } else if (std.mem.startsWith(u8, next_line, "//")) {
+            resultant_comment.appendSlice(allocator, next_line[3..]) catch return null;
+        }
+    }
+
+    return resultant_comment.toOwnedSlice(allocator) catch return null;
 }
 
 test "make_all_function_body_empty" {
